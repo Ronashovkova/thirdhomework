bbae994f4f1c32d94e6e3bd48f1b0b4e6062e870
Ronashovkova <rona_t@icloud.com>
2019-12-05 23:40:10 +0200

    class Application added

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,10 +1,19 @@
-package test;
+package main;
+import main.car.Car;
 import main.circle.*;
 
-public class TestCircle {
+public class Application {
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         System.out.println("The area of the " + (circle.getNumber()) + " is " +
                 (String.format("%.2f", circle.calculateArea())));
+
+
+        Car mercedes = new Car("Mercedes", 2.0);
+
+        mercedes.startEngine();
+        mercedes.drive();
+        mercedes.keepSpeed(60);
+        mercedes.stopEngine();
     }
 }
